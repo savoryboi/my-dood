99f58da153797eea7d861697cc00133f578bd8d0
KappaMustafa <steef136@gmail.com>
2022-08-31 15:03:11 -0500

    starter files

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+null
+  );
+}
+
+export default App;
---
+++ server/config/connection.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const URL = process.env.ATLAS_CONNECT_URL ? process.env.ATLAS_CONNECT_URL : 'mongodb://localhost:27017/my-dood'
+
+mongoose.connect(URL);
+
+module.exports = mongoose.connection;

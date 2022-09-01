785c57599595921bcc53aa345cf3be171117c3fc
savoryboi <andbjerk@yahoo.com>
2022-09-01 15:09:01 -0500

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import Timeline from "./components/Timeline";
 import FriendsList from "./components/FriendsList";
 import Draw from "./components/Draw";
+import Landing from './pages/Landing';
+
 function App() {
   return (
     <div>
       <Header />
-      
+
     </div>
 
   )

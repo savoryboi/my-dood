0d075ce5b06feb0eae35dbc8e3a6ed884c2b5679
savoryboi <andbjerk@yahoo.com>
2022-09-01 11:38:32 -0500

    typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        email: String!
+        friends: [User]
+    }
+
+    type Post {
+        _id: ID!
+        img: String!
+        text: String!
+        # user_id??? need to know what properties on here
+    }
+
+    type Auth {
+        user: User
+        token: ID
+    }
+
+    type Query {
+        getPosts: [Post]
+    }
+
+    type Mutation {
+        addUser(email: String!, password: String!): Auth
+        addPost(img: String!, text: String!): Post
+    }
+
+
+`
+
+module.exports = typeDefs;

8e651f8121e16c5f835ae8ec71f0290cb6146fc4
savoryboi <andbjerk@yahoo.com>
2022-09-02 10:57:09 -0500

    back to main

--- client/src/App.js
+++ client/src/App.js
@@ -8,46 +8,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import FriendProfile from "./pages/FriendProfile";
 import AuthForm from "./pages/AuthForm";
-<<<<<<< HEAD
-import Protect from './components/Protect.js';
-
-
-
-function App() {
-  return (
-    <div>
-
-    <Header logo={logo} user={user} />
-
-    <Routes>
-      <Route path="/" element={<Protect>
-        <Landing user={user} />
-      </Protect>} />
-
-      <Route path="/AuthForm" element={<Protect>
-        <AuthForm setUser={setUser} />
-      </Protect>} />
-
-      <Route path="/Profile" element={<Protect>
-      </Protect>} />
-
-
-      {/* if logged in then landing page becomes home page */}
-      <Route path="/" element={<Protect>
-        <Home />
-      </Protect>} />
-
-      <Route path="/FriendProfile" element={<Protect>
-        <FriendProfile />
-      </Protect>} />
-      
-      <Route path="/Draw" element={<Protect>
-        <Draw />
-      </Protect>} />
-    </Routes>
-  </div>
-=======
-import Landing from "./pages/Landing";
 
 function App() {
   return (
@@ -98,7 +58,6 @@ function App() {
         />
       </Routes> */}
     </div>
->>>>>>> 1a3bd6e267f871dc066a39159ac3dc7eff329eaa
   );
 }
 

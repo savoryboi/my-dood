b9b89da18540790cee0ac5bcecee7f90bd40664f
Derjbass <Derek.j.bass@gmail.com>
2022-09-06 12:16:57 -0500

    word of the day working

--- client/src/components/Protect.js
+++ client/src/components/Protect.js
@@ -5,7 +5,6 @@ import { useNavigate, useLocation } from "react-router-dom";
 function Protect(props) {
   const navigate = useNavigate();
   const location = useLocation();
-  console.log(props);
   useEffect(() => {
     if (
       !isAuthenticated() &&

57d07541a1a12d5eec2ecb8a13f514f8e883e534
palmersola <palmersola@yahoo.com>
2022-09-06 21:27:29 -0500

    to heroku, and beyond

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import { useState } from "react";
-// import { isAuthenticated } from "./utils/auth";
 import Protect from "./components/Protect";
 import AuthForm from "./pages/AuthForm/AuthForm";
 import Draw from "./pages/Draw/Draw";
 import Profile from "./pages/profiles/Profile";
-import Timeline from "./components/Timeline/Timeline";
+import Timeline from "./pages/Timeline/Timeline";
 import Search from "./pages/Search/Search";
 import Word from "./components/Word";
 import { Routes, Route } from "react-router-dom";
